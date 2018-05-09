f82bd0b67acafc5d170124b85f2460a00b222485
drxaos <vladimir.p.polyakov@gmail.com>
2018-05-09 22:48:21 +0500

    фикс интерфейса, отложненое восстановление капусты

--- src/main/java/com/github/drxaos/zc/ScoreManager.java
+++ src/main/java/com/github/drxaos/zc/ScoreManager.java
@@ -1,9 +1,12 @@
 package com.github.drxaos.zc;
 
+import java.util.ArrayList;
+
 public class ScoreManager implements Manager {
     Db db;
     Game game;
     Level level;
+    ArrayList<Long> add = new ArrayList<>();
 
     public ScoreManager(Db db, Game game, Level level) {
         this.db = db;
@@ -14,16 +17,17 @@ public class ScoreManager implements Manager {
     @Override
     public void manage(Game.State state) {
 
-        int g = 0;
-
         // Сохраняем очки
         while (state.hasScores()) {
             Game.Score score = state.nextScore();
             db.incScore(score.zid);
-            g++;
+            add.add(System.currentTimeMillis() + 1500);
         }
 
         // Обновляем капусту
-        level.addG(state, g);
+        if (add.size() > 0 && add.get(0) < System.currentTimeMillis()) {
+            add.remove(0);
+            level.addG(state, 1);
+        }
     }
 }
